8e84d8ab3b140d6102de167565d6a648cb10b6c0
Eddie Maune <eddie.maune@vcontractor.co.za>
2020-05-07 23:00:24 +0200

    quick fix

--- js/main.js
+++ js/main.js
@@ -8,22 +8,22 @@ $(document).ready(function () {
 	var micctx = micaudio.getContext("2d");
 	micctx.fillStyle = "blue";
 
-	var incaudio = document.getElementById("incaudio");
-	var incctx = incaudio.getContext("2d");
-	incctx.fillStyle = "#FF0000";
+	// var incaudio = document.getElementById("incaudio");
+	// var incctx = incaudio.getContext("2d");
+	// incctx.fillStyle = "#FF0000";
 
 	onMicRawAudio = function (audioData, soundcardSampleRate) { //Data right after mic input
 		micctx.clearRect(0, 0, micaudio.width, micaudio.height);
 		for (var i = 0; i < audioData.length; i++) {
-			micctx.fillRect(i, audioData[i] * 100 + 100, 1, 1);
+			micctx.fillRect(i, audioData[i] * 100 + 300, 1, 1);
 		}
 		return audioData;
 	}
 
 	onUserDecompressedAudio = function (audioData, userId, sampleRate, bitRate) { //Called when user audiodata coming from the client
-		incctx.clearRect(0, 0, incaudio.width, incaudio.height);
+		micctx.clearRect(0, 0, incaudio.width, incaudio.height);
 		for (var i = 0; i < audioData.length; i++) {
-			incctx.fillRect(i, audioData[i] * 100 + 100, 1, 1);
+			micctx.fillRect(i, audioData[i] * 100 + 100, 1, 1);
 		}
 		return audioData;
 	}
