e3b7b65b26bf48976c305f29ad4146dd06f78416
Eddie Maune <eddie.maune@vcontractor.co.za>
2020-05-31 16:58:18 +0200

    Changed back sample rate

--- js/voip.js
+++ js/voip.js
@@ -2,7 +2,7 @@ var socketIO = io.connect('https://circles-rtc.herokuapp.com/');
 
 var soundcardSampleRate = null; //Sample rate from the soundcard (is set at mic access)
 var mySampleRate = 12000; //Samplerate outgoing audio (common: 8000, 12000, 16000, 24000, 32000, 48000)
-var myBitRate = 32; //8,16,32 - outgoing bitrate
+var myBitRate = 16; //8,16,32 - outgoing bitrate
 var myMinGain = 6 / 100; //min Audiolvl
 var micAccessAllowed = false; //Is set to true if user granted access
 var chunkSize = 1024;
